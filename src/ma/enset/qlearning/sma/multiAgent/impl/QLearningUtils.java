97b2df056d998aa0dcada0a8ff8393e753b4b1d7
Mohamed Lemine <mohamedleminlehcene@gmail.com>
2023-06-07 00:09:04 +0100

    ajouter un classe contient constant

---
+++ src/ma/enset/qlearning/sma/multiAgent/impl/QLearningUtils.java
@@ -0,0 +1,23 @@
+package ma.enset.qlearning.sma.multiAgent.impl;
+
+import com.sun.org.apache.bcel.internal.generic.FALOAD;
+
+public class QLearningUtils {
+    public static boolean FINISHED=false;
+    public static final  double ALPHA=0.1;
+    public static final double GAMMA=0.9;
+    public static final int MAX_EPOCH=20000;
+    public static final int GRID_SIZE=3;
+    public static final int ACTION_SIZE=4;
+
+/*
+    //private boolean finished = false;
+    private final double ALPHA = 0.1;
+    private final double GAMMA = 0.9;
+    private final int MAX_EPOCH = 20000;
+    private final int GRID_SIZE = 3;
+    private final int ACTION_SIZE = 4;
+ */
+
+}
+
