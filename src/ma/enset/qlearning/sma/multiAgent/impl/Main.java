0b973488b8de6180c7d22671aedecd68de368c16
Mohamed Lemine <mohamedleminlehcene@gmail.com>
2023-06-07 16:15:19 +0100

    ajouter javaFX

--- src/ma/enset/qlearning/sma/multiAgent/impl/Main.java
+++ src/ma/enset/qlearning/sma/multiAgent/impl/Main.java
@@ -1,11 +1,103 @@
 package ma.enset.qlearning.sma.multiAgent.impl;// Main.java
 import jade.core.Profile;
 import jade.core.ProfileImpl;
+import jade.core.Runtime;
 import jade.wrapper.AgentContainer;
 import jade.wrapper.AgentController;
+import jade.wrapper.ControllerException;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class Main  extends Application {
+    private Button runButton;
+    private ObservableList<String> data = FXCollections.observableArrayList();
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
 
 
-public class Main {
+        /*
+        primaryStage.setTitle("QLearning Application");
+
+        runButton = new Button("Run QLearning");
+        runButton.setOnAction((event -> {
+            try {
+                startContent();
+            } catch (ControllerException e) {
+                throw new RuntimeException(e);
+            }
+        }));
+
+        VBox root = new VBox(runButton);
+        Scene scene = new Scene(root, 200, 100);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+         */
+
+
+        //Interface JavaFX
+        BorderPane root = new BorderPane();
+
+        Button buttonEnvoie =  new Button("Execute Algorithme QLearning Avec SMA");
+        buttonEnvoie.setPrefWidth(400);
+        buttonEnvoie.setPrefHeight(30);
+
+        HBox hBox = new HBox();
+        hBox.setAlignment(Pos.CENTER);
+        hBox.setSpacing(10);
+        hBox.setPadding(new Insets(10));
+        hBox.setBackground(new Background(new BackgroundFill(Color.ORANGE,null,null)));
+        hBox.getChildren().add(buttonEnvoie);
+        root.setBottom(hBox);
+
+        VBox vBox= new VBox();
+        vBox.setSpacing(10);
+        vBox.setPadding(new Insets(10));
+
+        root.setCenter(vBox);
+
+        Scene scene = new Scene(root,800,600);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("QLearning Application");
+        primaryStage.show();
+
+
+        buttonEnvoie.setOnAction((event -> {
+            try {
+                startContent();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }));
+
+    }
+
+    public void startContent() throws ControllerException{
+        Runtime runtime = Runtime.instance();Profile mainAgentProfile = new ProfileImpl();
+        AgentContainer mainAgentContainer = runtime.createMainContainer(mainAgentProfile);
+        try {
+            AgentController mainAgentController = mainAgentContainer.createNewAgent("MainAgentN",MainAgentN.class.getName(), null);
+            mainAgentController.start();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    /*
     public static void main(String[] args) {
         jade.core.Runtime rt = jade.core.Runtime.instance();
         Profile mainAgentProfile = new ProfileImpl();
@@ -17,5 +109,6 @@ public class Main {
             throw new RuntimeException(e);
         }
     }
+     */
 }
 
