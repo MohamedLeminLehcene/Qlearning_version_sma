d9f75704256014712fab8f78f32f5cf2bdc8df05
Mohamed Lemine <mohamedleminlehcene@gmail.com>
2023-06-07 16:55:15 +0100

    modifier class MainAgentN

--- src/ma/enset/qlearning/sma/multiAgent/impl/MainAgentN.java
+++ src/ma/enset/qlearning/sma/multiAgent/impl/MainAgentN.java
@@ -3,16 +3,26 @@ package ma.enset.qlearning.sma.multiAgent.impl;
 import jade.core.Agent;
         import jade.core.behaviours.CyclicBehaviour;
 import jade.core.behaviours.OneShotBehaviour;
+import jade.gui.GuiAgent;
+import jade.gui.GuiEvent;
 import jade.lang.acl.ACLMessage;
 import jade.wrapper.AgentController;
 
-public class MainAgentN  extends Agent {
+public class MainAgentN  extends GuiAgent {
+    private Main main;
     private int agent1ResultCount = 0;
     private int agent2ResultCount = 0;
 
     protected void setup() {
+        main = (Main)getArguments()[0];
+        main.setMainAgentN(this);
         addBehaviour(new StartAgentBehaviour());
         addBehaviour(new ReceiveResultBehaviour());
+    }
+
+    @Override
+    protected void onGuiEvent(GuiEvent guiEvent) {
+
     }
 
 
@@ -31,18 +41,24 @@ public class MainAgentN  extends Agent {
             }
         }
     }
-
     private class ReceiveResultBehaviour extends CyclicBehaviour {
         public void action() {
             ACLMessage message = myAgent.receive();
             if (message != null) {
                 String agentName = message.getSender().getLocalName();
                 String result = message.getContent();
+                System.out.println(result);
                 if (agentName.equals("Agent1")) {
+                    main.showResult(result);
+
                     System.out.println("Agent1 Result:\n" + result);
+
                     agent1ResultCount++;
                 } else if (agentName.equals("Agent2")) {
+
                     System.out.println("Agent2 Result:\n" + result);
+
+                    main.showResult(result);
                     agent2ResultCount++;
                 }
 
